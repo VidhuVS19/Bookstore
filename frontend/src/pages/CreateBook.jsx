54eec8884f52688dfefc9262823537b80282ddfa
Vidhu Vikas Singh <vidhu19122002@gmail.com>
2024-03-04 10:34:24 +0530

    third commit

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import { useNavigate } from 'react-router-dom';
