f2f986fa3ca38ead51747da3a5c5c819d2f5c8fb
Vidhu Vikas Singh <vidhu19122002@gmail.com>
2024-03-19 23:42:22 +0530

    Bookstore project completed

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
 
 const EditBook = () => {
   const [title, setTitle] = useState('');
@@ -10,6 +11,7 @@ const EditBook = () => {
   const [publishYear, setPublishYear] = useState('');
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
   const { _id } = useParams();
   useEffect(() => {
     setLoading(true);
@@ -37,11 +39,13 @@ const EditBook = () => {
     .put(`http://localhost:5555/books/${_id}`,data)
     .then(() => {
       setLoading(false);
+      enqueueSnackbar('Book Edited Successfully', { variant: 'success' });
       navigate('/');
     })
     .catch((error) => {
       setLoading(false);
-      alert('An error occured. Please check console');
+      // alert('An error occured. Please check console');
+      enqueueSnackbar('Error', { variant: 'error' });
       console.log(error);
     });
   };
